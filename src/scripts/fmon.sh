8cfa0403ef64aa8a81a5e57cddd7ab38025203b0
Martín Fernández <paziusss@gmail.com>
2018-02-12 02:57:23 +0100

    Update fmon.sh

--- src/scripts/fmon.sh
+++ src/scripts/fmon.sh
@@ -4,15 +4,14 @@
 
 [ -e /mnt/onboard/.kobo/version ] || exit 1
 
-SD_ROOT=/mnt/onboard
-ICONS_DIR="${SD_ROOT}/icons"
-SCRIPTS_DIR="${SD_ROOT}/.adds"
-LOGFILE="/tmp/fmon.log"
+ICONS_DIR="/mnt/onboard/icons"
+SCRIPTS_DIR="/mnt/onboard/.adds"
 
-echo "[fmon]: starting on FW: ${firmware_version}" >> "$LOGFILE"
 for icon in "${ICONS_DIR}"/*.png; do
     app=$(basename "$icon" .png)
     script="${SCRIPTS_DIR}/${app}/${app}.sh"
-    echo "[fmon]:registering new trigger: ${icon}, linked with ${script}" >> "$LOGFILE"
-    /sbin/fmon "$icon" "$script" > "$LOGFILE" 2>&1 &
+    logfile="/tmp/fmon-${app}.log"
+    
+    echo "[fmon]:registering new trigger: ${icon}, linked with ${script}" >> $logfile"
+    /sbin/fmon "$icon" "$script" >> "$logfile" 2>&1 &
 done
